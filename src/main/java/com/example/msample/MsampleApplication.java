c58e4bf152a255b562977f146ede0a77262475a0
tkc1009 <tkc1009@users.noreply.github.com>
2021-11-08 01:04:33 +0900

    Spring Initializr

---
+++ src/main/java/com/example/msample/MsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.msample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsampleApplication.class, args);
+	}
+
+}
